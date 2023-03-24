f5d578b2ddd192d49e6b2727fc3c8ed03f9a6abb
BlackTea13 <robertblum2001@gmail.com>
2023-03-24 17:09:08 +0700

    added message

--- src/simple_app/app.py
+++ src/simple_app/app.py
@@ -23,7 +23,7 @@ migrate = Migrate(app, db)
 
 @app.route("/")
 def index():
-    return "hello world bye karan!"
+    return "hello world bye karan!!"
 
 
 app.add_url_rule("/todo", methods=["GET"], view_func=todo_api.list_todo)
